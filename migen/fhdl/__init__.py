da602265910d670b4f1adecf147edcb585436458
Alain Péteut <alain.peteut@yahoo.com>
2012-09-02 01:05:51 +0200

    __init__.py: fix to get correct module name

--- migen/fhdl/__init__.py
+++ migen/fhdl/__init__.py
@@ -3,6 +3,7 @@
 # vim:noexpandtab:ts=8:sw=8:si:smarttab:tw=80
 
 import sys
+import os
 from migen.fhdl.verilog import convert as verilog_convert
 from migen.fhdl.vhdl import convert as vhdl_convert
 
@@ -31,7 +32,7 @@ def main():
 		default=sys.stdin)
 
 	globals()['_args'] = parser.parse_args()
-	globals()['__name__'] = _args.infile.name
+	globals()['__name__'] = os.path.split(_args.infile.name)[-1].split('.')[0]
 
 	exec(compile(_args.infile.read(), _args.infile.name, 'exec'),
 		globals(),
