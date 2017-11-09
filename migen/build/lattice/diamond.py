ed934f2796a180b9a347205fc49d5be692854534
William D. Jones <thor0505@comcast.net>
2017-11-09 05:45:31 -0500

    build/lattice: Refactor special_overrides as toolchain attributes.

--- migen/build/lattice/diamond.py
+++ migen/build/lattice/diamond.py
@@ -80,6 +80,8 @@ def _run_diamond(build_name, source, ver=None):
 class LatticeDiamondToolchain:
     attr_translate = DummyAttrTranslate()
 
+    special_overrides = common.diamond_special_overrides
+
     def build(self, platform, fragment, build_dir="build", build_name="top",
               toolchain_path="/opt/Diamond", run=True):
         os.makedirs(build_dir, exist_ok=True)
