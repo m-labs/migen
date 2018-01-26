1caf61d5e44dbb60491574bf31601cd080d09ad0
whitequark <whitequark@whitequark.org>
2018-01-26 13:48:32 +0000

    sim: write FSM states symbolically to VCD files.

--- migen/sim/vcd.py
+++ migen/sim/vcd.py
@@ -28,7 +28,15 @@ class VCDWriter:
         self.signal_values = dict()
         self.t = 0
 
-    def _write_value(self, f, signal, value):
+    def _get_code(self, signal):
+        try:
+            code = self.codes[signal]
+        except KeyError:
+            code = next(self.codegen)
+            self.codes[signal] = code
+        return code
+
+    def _write_primitive_value(self, f, signal, value):
         l = len(signal)
         if value < 0:
             value += 2**l
@@ -36,16 +44,24 @@ class VCDWriter:
             fmtstr = "b{:0" + str(l) + "b} {}\n"
         else:
             fmtstr = "{}{}\n"
-        try:
-            code = self.codes[signal]
-        except KeyError:
-            code = next(self.codegen)
-            self.codes[signal] = code
+        code = self._get_code(signal)
         f.write(fmtstr.format(value, code))
 
+    def _write_enum_value(self, f, signal, value):
+        val = "b"
+        for c in signal._enumeration[value].encode():
+            val += "{:08b}".format(c)
+        code = self._get_code(signal)
+        f.write("{} {}\n".format(val, code))
+
+    def _write_value(self, f, signal, value):
+        if hasattr(signal, "_enumeration"):
+            self._write_enum_value(self.buffer_file, signal, value)
+        else:
+            self._write_primitive_value(self.buffer_file, signal, value)
+
     def set(self, signal, value):
-        if (signal not in self.signal_values
-                or self.signal_values[signal] != value):
+        if signal not in self.signal_values or self.signal_values[signal] != value:
             self._write_value(self.buffer_file, signal, value)
             self.signal_values[signal] = value
 
@@ -59,8 +75,12 @@ class VCDWriter:
             ns = build_namespace(self.codes.keys())
             for signal, code in self.codes.items():
                 name = ns.get_name(signal)
-                out.write("$var wire {len} {code} {name} $end\n"
-                          .format(name=name, code=code, len=len(signal)))
+                if hasattr(signal, "_enumeration"):
+                    size = max([len(v) for v in signal._enumeration.values()])*8
+                else:
+                    size = len(signal)
+                out.write("$var wire {size} {code} {name} $end\n"
+                          .format(name=name, code=code, size=size))
             out.write("$dumpvars\n")
             for signal in self.codes.keys():
                 self._write_value(out, signal, signal.reset.value)
