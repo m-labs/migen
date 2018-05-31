df8662a275677a110b547cffb1a8308cf606bb74
William D. Jones <thor0505@comcast.net>
2018-05-31 10:51:04 -0400

    platforms/tinyfpga_a: Implement agreed-upon nearest_freq algorithm.

--- migen/build/platforms/tinyfpga_a.py
+++ migen/build/platforms/tinyfpga_a.py
@@ -32,7 +32,7 @@ class MachClock(Module):
     # Diamond will complain that the frequency of the oscillator doesn't match
     # if it doesn't match one of the below frequencies. Since we tend to deal
     # with periods in nanoseconds, choose the closest frequency (in MHz)
-    # without going over. These numbers were extracted from
+    # within +/-5%. These numbers were extracted from
     # "MachXO2 sysCLOCK PLL Design and Usage Guide"
     supported_freqs = [
         2.08, 2.15, 2.22, 2.29, 2.38, 2.46, 2.56, 2.66, 2.77, 2.89,
@@ -52,10 +52,31 @@ class MachClock(Module):
                                   )
 
     def nearest_freq(self, period):
-        i = bisect.bisect_right(self.supported_freqs, 1000.0/period)
-        if i:
-            return self.supported_freqs[i-1]
-        raise ValueError("Clock period out of range for internal oscillator.")
+        target_freq = 1000.0/period
+
+        # A None match is defined to not be in range. Handles special cases
+        # described below.
+        def in_range(actual, err=0.05):
+            return abs((target_freq - actual)/target_freq) <= err if actual else False
+
+        # Agreed-upon algorithm:
+        # https://github.com/m-labs/migen/pull/111#discussion_r189687411
+        i = bisect.bisect_right(self.supported_freqs, target_freq)
+
+        # Special case: desired freq was < 2.08, but could still within +5%
+        lo_match = self.supported_freqs[i-1] if i else None
+
+        # Special case: desired freq was > 133.00, but could still within -5%
+        hi_match = self.supported_freqs[i] if i < len(self.supported_freqs) else None
+
+        if in_range(lo_match):
+            selected_freq = lo_match
+        elif in_range(hi_match):
+            selected_freq = hi_match
+        else:
+            raise ValueError("Clock period out of range (-/+5%) for internal oscillator.")
+
+        return selected_freq
 
 
 class Platform(LatticePlatform):
