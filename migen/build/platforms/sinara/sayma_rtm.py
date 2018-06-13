e5cabe1c6dbc55bc6b9b55a703bdc3826679370e
Sebastien Bourdeauducq <sb@m-labs.hk>
2018-06-13 19:20:27 +0800

    sayma_rtm: fix I/O bank voltages

--- migen/build/platforms/sinara/sayma_rtm.py
+++ migen/build/platforms/sinara/sayma_rtm.py
@@ -3,12 +3,12 @@ from migen.build.xilinx import XilinxPlatform
 
 
 _io = [
-    ("clk50", 0, Pins("E15"), IOStandard("LVCMOS25")),
+    ("clk50", 0, Pins("E15"), IOStandard("LVCMOS33")),
 
     ("serial", 0,
         Subsignal("tx", Pins("C16")),
         Subsignal("rx", Pins("B17")),
-        IOStandard("LVCMOS25")
+        IOStandard("LVCMOS33")
     ),
 
     ("amc_rtm_serwb", 0,
@@ -18,7 +18,7 @@ _io = [
         Subsignal("tx_n", Pins("U17")),  # rtm_fpga_lvds2_n
         Subsignal("rx_p", Pins("R16"), Misc("DIFF_TERM=TRUE")),  # rtm_fpga_lvds1_p
         Subsignal("rx_n", Pins("R17"), Misc("DIFF_TERM=TRUE")),  # rtm_fpga_lvds1_n
-        IOStandard("LVDS_25")
+        IOStandard("LVDS_18")
     ),
 
     # HMC clocking chips (830 and 7043)
@@ -30,100 +30,100 @@ _io = [
         Subsignal("cs_n", Pins("C8 D16"), Misc("PULLDOWN=TRUE")),
         Subsignal("mosi", Pins("B16"), Misc("PULLDOWN=TRUE")),
         Subsignal("miso", Pins("D9"), Misc("PULLDOWN=TRUE")),
-        IOStandard("LVCMOS25")
+        IOStandard("LVCMOS33")
     ),
-    ("hmc7043_reset", 0, Pins("E17"), IOStandard("LVCMOS25")),
+    ("hmc7043_reset", 0, Pins("E17"), IOStandard("LVCMOS33")),
 
     # clock mux
-    ("clk_src_ext_sel", 0, Pins("P15"), IOStandard("LVCMOS25")),
-    ("ref_clk_src_sel", 0, Pins("J14"), IOStandard("LVCMOS25")),
-    ("dac_clk_src_sel", 0, Pins("P16"), IOStandard("LVCMOS25")),
-    ("ref_lo_clk_sel", 0, Pins("L18"), IOStandard("LVCMOS25")),
+    ("clk_src_ext_sel", 0, Pins("P15"), IOStandard("LVCMOS18")),
+    ("ref_clk_src_sel", 0, Pins("J14"), IOStandard("LVCMOS18")),
+    ("dac_clk_src_sel", 0, Pins("P16"), IOStandard("LVCMOS18")),
+    ("ref_lo_clk_sel", 0, Pins("L18"), IOStandard("LVCMOS18")),
 
     # DACs
-    ("ad9154_rst_n", 0, Pins("U15"), IOStandard("LVCMOS25")),
+    ("ad9154_rst_n", 0, Pins("U15"), IOStandard("LVCMOS18")),
     ("ad9154_spi", 0,
         Subsignal("clk", Pins("T13"), Misc("PULLDOWN=TRUE")),
         Subsignal("cs_n", Pins("U14"), Misc("PULLUP=TRUE")),
         Subsignal("mosi", Pins("V17"), Misc("PULLDOWN=TRUE")),
         Subsignal("miso", Pins("R13"), Misc("PULLDOWN=TRUE")),
-        IOStandard("LVCMOS25")
+        IOStandard("LVCMOS18")
     ),
-    ("ad9154_txen", 0, Pins("V16 U16"), IOStandard("LVCMOS25")),
+    ("ad9154_txen", 0, Pins("V16 U16"), IOStandard("LVCMOS18")),
     ("ad9154_spi", 1,
         Subsignal("clk", Pins("J15"), Misc("PULLDOWN=TRUE")),
         Subsignal("cs_n", Pins("K18"), Misc("PULLUP=TRUE")),
         Subsignal("mosi", Pins("J18"), Misc("PULLDOWN=TRUE")),
         Subsignal("miso", Pins("J16"), Misc("PULLDOWN=TRUE")),
-        IOStandard("LVCMOS25")
+        IOStandard("LVCMOS18")
     ),
-    ("ad9154_txen", 1, Pins("L17 L14"), IOStandard("LVCMOS25")),
+    ("ad9154_txen", 1, Pins("L17 L14"), IOStandard("LVCMOS18")),
 
     # Allaki
-    ("allaki0_rfsw0", 1, Pins("R2"), IOStandard("LVCMOS25")),
-    ("allaki0_rfsw1", 1, Pins("N4"), IOStandard("LVCMOS25")),
-    ("allaki1_rfsw0", 1, Pins("R5"), IOStandard("LVCMOS25")),
-    ("allaki1_rfsw1", 1, Pins("V4"), IOStandard("LVCMOS25")),
-    ("allaki2_rfsw0", 1, Pins("B15"), IOStandard("LVCMOS25")),
-    ("allaki2_rfsw1", 1, Pins("B12"), IOStandard("LVCMOS25")),
-    ("allaki3_rfsw0", 1, Pins("L4"), IOStandard("LVCMOS25")),
-    ("allaki3_rfsw1", 1, Pins("J4"), IOStandard("LVCMOS25")),
+    ("allaki0_rfsw0", 1, Pins("R2"), IOStandard("LVCMOS33")),
+    ("allaki0_rfsw1", 1, Pins("N4"), IOStandard("LVCMOS33")),
+    ("allaki1_rfsw0", 1, Pins("R5"), IOStandard("LVCMOS33")),
+    ("allaki1_rfsw1", 1, Pins("V4"), IOStandard("LVCMOS33")),
+    ("allaki2_rfsw0", 1, Pins("B15"), IOStandard("LVCMOS33")),
+    ("allaki2_rfsw1", 1, Pins("B12"), IOStandard("LVCMOS33")),
+    ("allaki3_rfsw0", 1, Pins("L4"), IOStandard("LVCMOS33")),
+    ("allaki3_rfsw1", 1, Pins("J4"), IOStandard("LVCMOS33")),
 
     ("allaki0_att0", 1,
         Subsignal("le", Pins("U2")),
         Subsignal("sin", Pins("T4")),
         Subsignal("clk", Pins("V3")),
         Subsignal("rst_n", Pins("R3")),
-        IOStandard("LVCMOS25")
+        IOStandard("LVCMOS33")
     ),
     ("allaki0_att1", 1,
         Subsignal("le", Pins("P1")),
         Subsignal("sin", Pins("P3")),
         Subsignal("clk", Pins("N3")),
         Subsignal("rst_n", Pins("M4")),
-        IOStandard("LVCMOS25")
+        IOStandard("LVCMOS33")
     ),
     ("allaki1_att0", 1,
         Subsignal("le", Pins("U7")),
         Subsignal("sin", Pins("R6")),
         Subsignal("clk", Pins("V8")),
         Subsignal("rst_n", Pins("R7")),
-        IOStandard("LVCMOS25")
+        IOStandard("LVCMOS33")
     ),
     ("allaki1_att1", 1,
         Subsignal("le", Pins("T3")),
         Subsignal("sin", Pins("U5")),
         Subsignal("clk", Pins("P6")),
         Subsignal("rst_n", Pins("U4")),
-        IOStandard("LVCMOS25")
+        IOStandard("LVCMOS33")
     ),
     ("allaki2_att0", 1,
         Subsignal("le", Pins("C13")),
         Subsignal("sin", Pins("E16")),
         Subsignal("clk", Pins("D14")),
         Subsignal("rst_n", Pins("A15")),
-        IOStandard("LVCMOS25")
+        IOStandard("LVCMOS33")
     ),
     ("allaki2_att1", 1,
         Subsignal("le", Pins("D11")),
         Subsignal("sin", Pins("C14")),
         Subsignal("clk", Pins("A12")),
         Subsignal("rst_n", Pins("C12")),
-        IOStandard("LVCMOS25")
+        IOStandard("LVCMOS33")
     ),
     ("allaki3_att0", 1,
         Subsignal("le", Pins("M2")),
         Subsignal("sin", Pins("N1")),
         Subsignal("clk", Pins("M6")),
         Subsignal("rst_n", Pins("L5")),
-        IOStandard("LVCMOS25")
+        IOStandard("LVCMOS33")
     ),
     ("allaki3_att1", 1,
         Subsignal("le", Pins("K5")),
         Subsignal("sin", Pins("L2")),
         Subsignal("clk", Pins("K2")),
         Subsignal("rst_n", Pins("J5")),
-        IOStandard("LVCMOS25")
+        IOStandard("LVCMOS33")
     ),
 ]
 
