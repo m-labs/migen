cf4c3ef5f1f83b74850ba0145e0bfe784bead7d6
whitequark <whitequark@whitequark.org>
2018-11-11 08:52:08 +0000

    build/lattice/diamond: translate `keep` and `no_retiming` attributes.

--- migen/build/lattice/diamond.py
+++ migen/build/lattice/diamond.py
@@ -114,7 +114,18 @@ def _run_script(script):
 
 
 class LatticeDiamondToolchain:
-    attr_translate = DummyAttrTranslate()
+    attr_translate = {
+        # FIXME: document
+        "keep": ("syn_keep", "true"),
+        "no_retiming": ("syn_no_retiming", "true"),
+        "async_reg": None,
+        "mr_ff": None,
+        "mr_false_path": None,
+        "ars_ff1": None,
+        "ars_ff2": None,
+        "ars_false_path": None,
+        "no_shreg_extract": None
+    }
 
     special_overrides = common.lattice_ecpx_special_overrides
 
