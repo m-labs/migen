681569163fd3231eaa0443e2b97f9c1b8c3b0e96
Sebastien Bourdeauducq <sb@m-labs.hk>
2019-05-18 23:49:23 +0800

    metlino: use same SDRAM constraints as Sayma

--- migen/build/platforms/sinara/metlino.py
+++ migen/build/platforms/sinara/metlino.py
@@ -13,7 +13,8 @@ _io = [
 
     ("ddram", 0,
         Subsignal("a", Pins(
-            "AE17 AL17 AG16 AG17 AD16 AH14 AD15 AK15 AF14 AF15 AL18 AL15 AE18 AJ15 AG14"),
+            "AE17 AL17 AG16 AG17 AD16 AH14 AD15 AK15",
+            "AF14 AF15 AL18 AL15 AE18 AJ15 AG14"),
             IOStandard("SSTL15_DCI")),
         Subsignal("ba", Pins("AF17 AD19 AD18"), IOStandard("SSTL15_DCI")),
         Subsignal("ras_n", Pins("AH19"), IOStandard("SSTL15_DCI")),
@@ -21,19 +22,35 @@ _io = [
         Subsignal("we_n", Pins("AG19"), IOStandard("SSTL15_DCI")),
         Subsignal("cs_n", Pins("AF18"), IOStandard("SSTL15_DCI")),
         Subsignal("dm", Pins("AD21 AE25 AJ21 AM21 AH26 AN26 AJ29 AL32"),
-            IOStandard("SSTL15_DCI")),
+            IOStandard("SSTL15_DCI"),
+            Misc("DATA_RATE=DDR")),
         Subsignal("dq", Pins(
-            "AE23 AG20 AF22 AF20 AE22 AD20 AG22 AE20 AJ24 AG24 AJ23 AF23 AH23 AF24 AH22 AG25 AL22 AL25 AM20 AK23 AK22 AL24 AL20 AL23 AM24 AN23 AN24 AP23 AP25 AN22 AP24 AM22 AH28 AK26 AK28 AM27 AJ28 AH27 AK27 AM26 AL30 AP29 AM30 AN28 AL29 AP28 AM29 AN27 AH31 AH32 AJ34 AK31 AJ31 AJ30 AH34 AK32 AN33 AP33 AM34 AP31 AM32 AN31 AL34 AN32"),
-            IOStandard("SSTL15_DCI")),
+            "AE23 AG20 AF22 AF20 AE22 AD20 AG22 AE20",
+            "AJ24 AG24 AJ23 AF23 AH23 AF24 AH22 AG25",
+            "AL22 AL25 AM20 AK23 AK22 AL24 AL20 AL23",
+            "AM24 AN23 AN24 AP23 AP25 AN22 AP24 AM22",
+            "AH28 AK26 AK28 AM27 AJ28 AH27 AK27 AM26",
+            "AL30 AP29 AM30 AN28 AL29 AP28 AM29 AN27",
+            "AH31 AH32 AJ34 AK31 AJ31 AJ30 AH34 AK32",
+            "AN33 AP33 AM34 AP31 AM32 AN31 AL34 AN32"),
+            IOStandard("SSTL15_DCI"),
+            Misc("ODT=RTT_40"),
+            Misc("DATA_RATE=DDR")),
         Subsignal("dqs_p", Pins("AG21 AH24 AJ20 AP20 AL27 AN29 AH33 AN34"),
-            IOStandard("DIFF_SSTL15_DCI")),
+            IOStandard("DIFF_SSTL15_DCI"),
+            Misc("ODT=RTT_40"),
+            Misc("DATA_RATE=DDR")),
         Subsignal("dqs_n", Pins("AH21 AJ25 AK20 AP21 AL28 AP30 AJ33 AP34"),
-            IOStandard("DIFF_SSTL15_DCI")),
-        Subsignal("clk_p", Pins("AE16"), IOStandard("DIFF_SSTL15_DCI")),
-        Subsignal("clk_n", Pins("AE15"), IOStandard("DIFF_SSTL15_DCI")),
+            IOStandard("DIFF_SSTL15_DCI"),
+            Misc("ODT=RTT_40"),
+            Misc("DATA_RATE=DDR")),
+        Subsignal("clk_p", Pins("AE16"), IOStandard("DIFF_SSTL15_DCI"), Misc("DATA_RATE=DDR")),
+        Subsignal("clk_n", Pins("AE15"), IOStandard("DIFF_SSTL15_DCI"), Misc("DATA_RATE=DDR")),
         Subsignal("cke", Pins("AL19"), IOStandard("SSTL15_DCI")),
         Subsignal("odt", Pins("AJ18"), IOStandard("SSTL15_DCI")),
-        Subsignal("reset_n", Pins("AJ14"), IOStandard("SSTL15"))
+        Subsignal("reset_n", Pins("AJ14"), IOStandard("SSTL15")),
+        Misc("SLEW=FAST"),
+        Misc("OUTPUT_IMPEDANCE=RDRV_40_40")
     )
 ]
 
