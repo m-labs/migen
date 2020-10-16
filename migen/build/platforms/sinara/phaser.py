39bcdf5c4d2f9d5816a9f07b5d9bd986fb6043d8
Robert Jördens <rj@quartiq.de>
2020-10-16 09:05:57 +0000

    phaser: move to sinara, remove load/flash tooling

--- migen/build/platforms/sinara/phaser.py
+++ migen/build/platforms/sinara/phaser.py
@@ -273,7 +273,7 @@ _extensions = [
 
 class Platform(XilinxPlatform):
     userid = 0xffffffff
-    def __init__(self, load=False, flash=False):
+    def __init__(self):
         XilinxPlatform.__init__(
                 self, "xc7a100t-fgg484-2", _ios, _connectors,
                 toolchain="vivado")
@@ -293,41 +293,3 @@ class Platform(XilinxPlatform):
             "set_property CFGBVS VCCO [current_design]",
             "set_property CONFIG_VOLTAGE 2.5 [current_design]",
         ])
-        if load or flash:
-            self.toolchain.additional_commands.extend([
-                "open_hw_manager",
-                "connect_hw_server",
-                "open_hw_target",
-                "current_hw_device [lindex [get_hw_devices] 0]",
-            ] + ([
-                "write_cfgmem -force -format MCS -size 8 -interface SPIx4 "
-                    "-loadbit \"up 0x0 {build_name}.bit\" {build_name}",
-                "create_hw_cfgmem -hw_device [current_hw_device] "
-                    "[lindex [get_cfgmem_parts "
-                    "{{s25fl128sxxxxxx0-spi-x1_x2_x4}}] 0]",
-                "set_property PROGRAM.BLANK_CHECK 0 [current_hw_cfgmem]",
-                "set_property PROGRAM.ERASE 1 [current_hw_cfgmem]",
-                "set_property PROGRAM.CFG_PROGRAM 1 [current_hw_cfgmem]",
-                "set_property PROGRAM.VERIFY 1 [current_hw_cfgmem]",
-                "set_property PROGRAM.CHECKSUM 0 [current_hw_cfgmem]",
-                "set_property PROGRAM.ADDRESS_RANGE "
-                    "{{use_file}} [current_hw_cfgmem]",
-                "set_property PROGRAM.FILES "
-                    "{{{build_name}.mcs}} [current_hw_cfgmem]",
-                "set_property PROGRAM.UNUSED_PIN_TERMINATION "
-                    "{{pull-none}} [current_hw_cfgmem]",
-                "create_hw_bitstream -hw_device [current_hw_device] "
-                    "[get_property PROGRAM.HW_CFGMEM_BITFILE "
-                    "[current_hw_device]]",
-                "program_hw_devices",
-                "program_hw_cfgmem",
-                "boot_hw_device -verbose [current_hw_device]"
-            ] if flash else [
-                "set_property PROGRAM.FILE {{{build_name}.bit}} "
-                   "[current_hw_device]",
-                "program_hw_devices",
-                # "refresh_hw_device",
-            ]) + [
-                "close_hw_target",
-                "close_hw_manager"
-            ])
