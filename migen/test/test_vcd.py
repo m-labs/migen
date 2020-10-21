f665f7c8ba920d7c8b6f8d0571b39d15f48464dd
Piotr Fryc <piotr.fryc@gmail.com>
2020-10-21 05:37:57 +0200

    Increase readability

--- migen/test/test_vcd.py
+++ migen/test/test_vcd.py
@@ -10,14 +10,14 @@ class VcdWriter(unittest.TestCase):
     def get_file_path(self, dir):
         return os.path.join(dir, self.id() + ".vcd")
 
-    def check_expectation(self, filename):
+    def check_expectation(self, filename, expectation):
         self.vcd.close()
         with open(filename, 'r') as f:
-            self.assertMultiLineEqual(self.expected_file, f.read())
+            self.assertMultiLineEqual(expectation, f.read())
 
     def test_empty(self):
 
-        self.expected_file = (
+        expected_content = (
             "$dumpvars\n"
             "$end\n"
             "#0\n"
@@ -26,11 +26,11 @@ class VcdWriter(unittest.TestCase):
         with tempfile.TemporaryDirectory() as dir:
             filename = self.get_file_path(dir)
             self.vcd = VCDWriter(filename)
-            self.check_expectation(filename)
+            self.check_expectation(filename, expected_content)
 
     def test_module_name(self):
 
-        self.expected_file = (
+        expected_content = (
             "$scope module name1 $end\n"
             "$enddefinitions $end\n"
             "$dumpvars\n"
@@ -41,4 +41,4 @@ class VcdWriter(unittest.TestCase):
         with tempfile.TemporaryDirectory() as dir:
             filename = self.get_file_path(dir)
             self.vcd = VCDWriter(filename, module_name="name1")
-            self.check_expectation(filename)
+            self.check_expectation(filename, expected_content)
