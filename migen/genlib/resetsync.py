e65cffe8bff5a7070e0f2e838540b3e580968a78
Wardstein <jonaseberhard@web.de>
2020-12-17 10:41:24 +0100

    Added documentation to AsyncResetSynchronizer

--- migen/genlib/resetsync.py
+++ migen/genlib/resetsync.py
@@ -4,8 +4,21 @@ from migen.fhdl.specials import Special
 
 __all__ = ["AsyncResetSynchronizer"]
 
-
 class AsyncResetSynchronizer(Special):
+    """AsyncResetSynchronizer
+    Add a reset signal to the provided ClockDomain synchronized into the desired ClockDomain.
+    Synchronizing is done by adding two D-Flip-Flops in series with the ClockDomain clock.
+    This way the "async_reset" input signal is brought through the two flip-flips into the provided ClockDomain.
+    The output of the second flip-flop is directly connected to the reset signal of the provided ClockDomain.
+    
+    Needs to be added to the specials list: self.specials += AsyncResetSynchronizer()
+    
+    cd : in
+        ClockDomain the reset signal shall be connected to
+    async_reset : in
+        Reset signal (potentially) asynchronous to the ClockDomain provided in parameter "cd".
+        Synchronized instance of the reset signal is connected to ClockDomain "rst" signal.
+    """
     def __init__(self, cd, async_reset):
         Special.__init__(self)
         self.cd = cd
